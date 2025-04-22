9d1ace88e57c473e2f98162881a917b15625c98b
Laknath1996 <ashwindesilva@yahoo.com>
2025-04-22 13:22:28 -0400

    Update

--- src/experiment.py
+++ src/experiment.py
@@ -1,4 +1,3 @@
-# %%
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
@@ -7,14 +6,12 @@ import re
 
 import numpy as np
 
-# %%
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct", torch_dtype=torch.float16)
 model.to(1)
 
 
-# %%
 def divide_to_chapter(text):
     chapter_pattern = r'(?i)^chapter\s+[a-z]+'
     titles = re.findall(chapter_pattern, text, re.MULTILINE)
@@ -35,15 +32,6 @@ def read_list_from_json(file_path):
     else:
         raise ValueError("The JSON file does not contain a list.")
     
-def extract_questions(generated_str):
-    match = re.search(r'\[.*?\]', generated_str, re.DOTALL)
-    json_str = match.group(0)
-    try:
-        return json.loads(json_str)
-    except (json.JSONDecodeError, AttributeError) as e:
-        print(f"Error decoding JSON: {e}")
-        return None
-
 def get_prompt(context, question, a, b, c, d):
     prompt = f"""
     You are given a context and a multiple-choice question. Choose the **best answer** from the options (A, B, C, D). Respond with only the **letter corresponding to the correct answer**.
@@ -71,78 +59,55 @@ def get_model_answer(prompt):
     response = tokenizer.decode(model_output[0][input_ids_length:], skip_special_tokens=True)
     return response
 
-# %%
+if __name__ == "__main__":
 
-book_path = 'hp/harry_potter_1.txt'
-questions_path = 'hp/harry_potter_1_question_sets.json'
-text = read_text_file(book_path)
-chapters = divide_to_chapter(text)
-question_sets = read_list_from_json(questions_path)
+    book_path = 'hp/harry_potter_1.txt'
+    questions_path = 'hp/harry_potter_1_question_sets.json'
+    text = read_text_file(book_path)
+    chapters = divide_to_chapter(text)
+    question_sets = read_list_from_json(questions_path)
 
-# %%
+    results = []
 
-results = []
-for question_set in question_sets:
-    history_upto = question_set['history_upto']
-    context_chapter = question_set['context']
-    context = chapters[context_chapter]
-    q_list = extract_questions(question_set['output'])
+    for question_set in question_sets:
+        history_upto = question_set['history_upto']
+        context_chapter = question_set['context']
+        context = chapters[context_chapter]
+        q_list = question_set['output']
 
-    truths = []
-    preds = []
-    for q in q_list:
+        truths = []
+        preds = []
+        error = None
 
-        question = q['question']
+        if q_list is not None:
+            for q in q_list:
 
-        try: 
-            true_answer = q['answer']
-        except KeyError:
-            true_answer = q['correct_answer']
+                question = q['question']
+                true_answer = q['answer']
+                a, b, c, d = q['A'], q['B'], q['C'], q['D']
+
+                prompt = get_prompt(context, question, a, b, c, d)
+                
+                model_answer = get_model_answer(prompt).strip()
+
+                print(f"t = {history_upto}, k = {context_chapter}, True answer: {true_answer}, Model answer: {model_answer}")
+
+                truths.append(true_answer)
+                preds.append(model_answer)
+
+            error = np.mean(np.array(truths) != np.array(preds))
         
-        if 'options' in q:        
-            a = q['options']['A']
-            b = q['options']['B']
-            c = q['options']['C']
-            d = q['options']['D']
-        elif 'choices' in q:    
-            a = q['choices']['A']
-            b = q['choices']['B']
-            c = q['choices']['C']
-            d = q['choices']['D']
-        elif 'A' in q:
-            a = q['A']
-            b = q['B']
-            c = q['C']
-            d = q['D']
-        else:
-            raise ValueError("No options or choices found in the question.")
-
-        prompt = get_prompt(context, question, a, b, c, d)
-        
-        model_answer = get_model_answer(prompt).strip()
-
-        print(f"True answer: {true_answer}, Model answer: {model_answer}")
-
-        truths.append(true_answer)
-        preds.append(model_answer)
-        # res.append(int(model_answer == true_answer))
-        # print(res)
-
-    error = np.mean(np.array(truths) != np.array(preds))
-    
-    results.append(
-        {
-            't': history_upto,
-            'k': context_chapter,
-            'error': error,
-            'truths': truths,
-            'preds': preds
-        }
-    )
+        results.append(
+            {
+                't': history_upto,
+                'k': context_chapter,
+                'error': error,
+                'truths': truths,
+                'preds': preds
+            }
+        )
 
 
-filename = 'results/results.json'
-with open(filename, 'w') as f:
-    json.dump(results, f, indent=4)
-
-# %%
+    filename = 'results/results.json'
+    with open(filename, 'w') as f:
+        json.dump(results, f, indent=4)
